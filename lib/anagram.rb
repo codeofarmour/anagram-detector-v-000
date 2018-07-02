4020ea35fde876704661b623cc71b8c6dc26b560
Terri Riggle <codeofarmour@outlook.com>
2018-07-02 20:47:35 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,8 @@
 # Your code goes here!
 class Anagram
   attr_accessor :anagrams
-end
 
-def initialize(anagrams)
-  @anagrams = anagrams
-end
+  def initialize(anagrams)
+    @anagrams = anagrams
+  end
 end
